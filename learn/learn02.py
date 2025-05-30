b3483ce67be7eea5eb89254eb1061e9bdf83f4bb
home <447807952@qq.com>
2025-05-30 11:57:53 +0800

    2

--- learn/learn02.py
+++ learn/learn02.py
@@ -44,11 +44,16 @@ def get_data(stock_code: str, start_time='20240101', end_time='20250101'):
 class MyStrategy(bt.Strategy):
     params = (
         ('period', 20),  # 均线周期
+        ('period2', 60),  # 参数2
     )
 
     def __init__(self):
         self.order = None
         self.ma = bt.indicators.MovingAverageSimple()
+        # SMA是backtrader通过元类注册的别名
+        self.ma1 = bt.indicators.SMA(self.data0, period=self.params.period2)
+        self.ma2 = bt.indicators.SMA(self.data1, period=self.params.period2)
+
 
         # 添加分析器
         self.analyzers.returns = bt.analyzers.Returns()
@@ -56,27 +61,47 @@ class MyStrategy(bt.Strategy):
 
     def next(self):
         if not self.position:
-            if self.datas[0].close[0] > self.ma[0]:
+            if self.data.close[0] > self.ma[0]:
                 # 如果当前价格高于20日均线，则买入
                 self.order = self.buy(size=100)
         else:
-            if self.datas[0].close[0] < self.ma[0]:
+            if self.data.close[0] < self.ma[0]:
                 # 如果当前价格低于20日均线，则卖出
                 self.order = self.sell(size=100)
+
+
+# 作为对比, 构建一个买入持有策略
+class HoldStrategy(bt.Strategy):
+    def __init__(self):
+        self.order = None
+
+    def start(self):
+        price = self.data.close[0]
+        commission = self.broker.getcommissioninfo(self.data).p
+
+        # 计算最大可买数量（考虑佣金+最小交易单位）
+        max_possible = self.broker.get_cash() / (price * (1 + commission.commission))
+        size = int(max_possible * 0.99)
+
+        self.buy(size=size)
 
 
 if __name__ == '__main__':
     # 3. 设置策略
     cerebro = bt.Cerebro()  # 创建大脑
 
-    start_time = '20170101'  # 回测开始时间
-    end_time = '20220101'  # 回测结束时间
+    start_time = '20250101'  # 回测开始时间
+    end_time = '20250501'  # 回测结束时间
     stock_code = '600519'  # 股票代码
     data = get_data(stock_code, start_time, end_time)  # 获取数据
+    data2 = get_data('600036', start_time, end_time)
 
-    cerebro.adddata(data)  # 添加数据
+    cerebro.adddata(data, 'maotai')  # 添加数据
+    cerebro.adddata(data2, 'zhaohang')
 
     cerebro.addstrategy(MyStrategy)  # 添加策略
+    print(cerebro.datas[0])
+    print(cerebro.datas[1])
 
     # 4. 设置broker
     cash = 100000  # 初始资金
